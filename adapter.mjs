d99db093a3d2cbf5ca8d40dc32a5c681975311b5
Michael Pröpster <imp@outlook.de>
2025-10-31 12:36:53 +0100

    trace

--- adapter.mjs
+++ adapter.mjs
@@ -150,7 +150,7 @@ class McpServer extends utils.Adapter {
 
 			// Nothing changed with respect to our indexing-relevant fields
 			if (prev.enabled === enabled && prev.description === description) {
-				this.log.debug(`[Kiwi Adapter] No index-relevant change for ${id}`);
+				this.log.trace(`[Kiwi Adapter] No index-relevant change for ${id}`);
 				return;
 			}
 
